3b8c9dc83bd69ceb7d636920991a9fc36bbd64de
Yaroslav <71347933+superhe11@users.noreply.github.com>
2024-06-23 23:29:28 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,6 @@ import pytz
 from datetime import datetime
 from telethon.sync import TelegramClient, functions
 
-# Функция для чтения конфигурационного файла
 def read_config(file_path):
     config = {}
     with open(file_path, 'r') as file:
@@ -12,10 +11,8 @@ def read_config(file_path):
             config[key] = value
     return config
 
-# Чтение конфигурации
 config = read_config('config.txt')
 
-# Извлечение параметров из конфигурации
 api_id = config['api_id']
 api_hash = config['api_hash']
 phone = config['phone']
@@ -23,11 +20,9 @@ update_interval = int(config['update_interval'])
 language = config['language']
 timezone = config['timezone']
 
-# Создаём клиента
 client = TelegramClient('session_name', api_id, api_hash)
 
 async def update_bio():
-    # Получение текущего времени в заданном часовом поясе
     tz = pytz.timezone(timezone)
     current_time = datetime.now(tz).strftime('%H:%M')
     
@@ -36,16 +31,13 @@ async def update_bio():
     elif language == 'ru':
         new_bio = f'Сейчас {current_time}'
     else:
-        new_bio = f'The current time is {current_time}'  # Значение по умолчанию
+        new_bio = f'The current time is {current_time}'
 
-    # Обновление BIO профиля
     await client(functions.account.UpdateProfileRequest(about=new_bio))
 
 async def main():
-    # Авторизация
     await client.start(phone)
     
-    # Получение текущего времени в заданном часовом поясе для ожидания первого изменения времени
     tz = pytz.timezone(timezone)
     last_minute = datetime.now(tz).minute
     
@@ -56,6 +48,5 @@ async def main():
             last_minute = current_minute
             time.sleep(update_interval)
 
-# Запуск клиента
 with client:
     client.loop.run_until_complete(main())
